3e23739323443d6cc7ea1813f6bade59f7dc2051
ignotum <asylumexulting@gmail.com>
2019-11-23 16:43:55 +0100

    Initial commit

---
+++ content/bookmarks.md
@@ -0,0 +1,11 @@
+---
+title: Bookmarks
+date: 2019-04-16T14:19:14+01:00
+layout : bookmarks  
+permalink: /bookmarks/
+ 
+ 
+---
+
+ ok
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,170 +1,152 @@
+
+
+var ias = $.ias({
+  container: ".links",
+  item: ".link-list__link",
+  pagination: ".load-more",
+  next: ".more" 
+});
+function menuToggle() {
+$('#menu-toggle').toggleClass('open');
+$('#nav-mobile').toggleClass('d-none');
+// $('body').toggleClass('no-scroll'); 
+$('.header__nav--main').toggle( );
+// $('.jsless-switchery').toggle( );
+// $(".header__logo").parent().css({position: 'absolute'});
+ $('.header__nav--main').toggleClass('huhu' );
+}
+// function ss() {
+// if (navigator.share) { 
+//      navigator.share({
+//         title: 'WebShare API Demo',
+//         url: '{{ .Params.link }}'
+//       }).then(() => {
+//         console.log('Thanks for sharing!');
+//       })
+//       .catch(console.error);
+//       } else {
+//           shareDialog.classList.add('is-open');
+//       }
+// }
+$('#menu-toggle, #nav-mobile').click(function () {
+menuToggle();
+});
+// $('.bt').click(function () {
+// $(".bookmark").toggle();
+
+// });
+$(window).scroll(function() {     
+var scroll = $(window).scrollTop();
+if (scroll > 0) {
+    $(".header").addClass("shadow");
+}
+else {
+    $(".header").removeClass("shadow");
+}
+}); 
+function darkColor() {
+return "#282828" ; 
+}
+function lightColor() {
+return "#D2E1FF" ;
+}
+function changeThemeColordark() {
+var metaThemeColor = document.querySelector("meta[name=theme-color]");
+metaThemeColor.setAttribute("content", darkColor());
+}
+function changeThemeColorlight() {
+var metaThemeColor = document.querySelector("meta[name=theme-color]");
+metaThemeColor.setAttribute("content", lightColor());
+}
+function darkmode(){
+$('body').addClass('dark-mode');
+localStorage.setItem("mode", "dark");
+$( "#switch" ).prop( "checked", true );
+$('.link-list__link').css("background-color","rgba(46, 45, 45, 0.8)"),
+changeThemeColordark();
+}
+function nodark(){
+    $('body').removeClass('dark-mode');
+    $('.link-list__link').css("background-color","rgb(243, 242, 242)"),
+    localStorage.setItem("mode", "light");
+    changeThemeColorlight();
+    }
+if(localStorage.getItem("mode")=="dark")
+
  
-const Bookmark = (dataObj) => {
-  observeData(dataObj)
+    darkmode();
+else
 
-  return {
-    data: dataObj
-  }
+nodark();
+$('#switch').change(function(){   
+if ($(this).prop('checked'))
+{
+    darkmode();
 
-  function makeReactive (obj, key) {
-    let val = obj[key]
-
-    Object.defineProperty(obj, key, {
-      get () {
-        return val
-      },
-      set (newVal) {
-        val = newVal
-        parseDOM(obj)
-      }
-    })
-  }
-
-  function observeData (obj) {
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        makeReactive(obj, key)
-      }
-    }
-
-    parseDOM(obj)
-  }
-
-  function parseDOM (obj) {
-    let count = obj.items.length
-    let dropdownNode = document.querySelector('.js-b-dropdown')
-
-    // update counter
-    document.querySelector('.js-b-counter').textContent = count
-
-    // render bookmark items
-    if (count === 0) {
-      dropdownNode.innerHTML = `
-      <p class="no-item">
-        <span>No bookmark items found.</span>
-      </p>
-      `
-    } else {
-      let html = ''
-      obj.items.forEach(item => {
-        html += `
-        <div class="link" >
-          <h1><a href="${item.link}" target="_blank" rel="nofollow noopener">${item.title}</h1>
-          <div class="meta">
-            <a href="http://${item.site}" target="_blank" rel="nofollow noopener">${item.site}</a> | <a href="#" class="js-b-dropdown-remove-bookmark" data-id="${item.link}">remove</a>
-          </div>
-        </div>
-        `
-      });
-      dropdownNode.innerHTML = html
-    }
-  }
 }
+else
+{
+    nodark();
 
-let bookmarkNode = document.querySelector('.bookmark')
-
-const showDropdown = () => {
-  bookmarkNode.classList.add('show')
 }
+});
 
-const hideDropdown = () => {
-  bookmarkNode.classList.remove('show')
+
+
+ias.extension(new IASNoneLeftExtension({text: '&#8226;'}));
+ias.extension(new IASSpinnerExtension({ src: '/img/darkspinner.svg', }));
+
+
+window.onload = function() {randomColor();}
+window.onclick = function() {randomColor();}
+setInterval(function(){randomColor();},3000) 
+
+function randomColor(){
+var randNumber = Math.random() * 11111111;
+var randHex = parseInt(randNumber).toString(16);
+var randColor = "#" + randHex;
+$('.header__logo, .cs').css('background-color', randColor);
+$('.active').css('color', randColor);
+$(".link-list__link").mouseover(function() {
+$(this).css("background-color", randColor);
+}).mouseout(function() {
+if(localStorage.getItem("mode")=="dark")
+
+$(this).css("background-color","rgba(46, 45, 45, 0.8)");
+else
+$(this).css("background-color","rgb(243, 242, 242)");
+});  
 }
+$('.link-list__link').mouseover(function() {
+$(this).find('.share-icon, .bookmark-icon').show()
+}).mouseout(function() { 
+    $('.share-icon, .bookmark-icon').hide();
+});
+$('.share-icon').click(function() {
 
-// // Generate octicons
-// let octiconsEl = document.querySelectorAll('.i-octicon')
-// octiconsEl.forEach((oct) => {
-//   let icon = oct.getAttribute('data-icon')
-//   if (octicons[icon]) {
-//     oct.innerHTML = octicons[icon].toSVG()
-//   }
-// })
+    $(this).closest('.share-box').find('.link-title,.btm').hide();
+    $(this).closest('.share-box').find('.share-icons').fadeIn();
+    
+});
 
-// Initialize the bookmark
-let b = Bookmark({
-  items: localStorage.getItem('bookmark_items') ? JSON.parse(localStorage.getItem('bookmark_items')) : []
-})
+$( ".share-box" ).mouseleave(function() {
+$('.share-icons').hide();
 
-// Highlight the bookmark icon of added items.
-b.data.items.forEach(item => {
-  document.querySelectorAll(`[data-link="${item.link}"]`).forEach(node => {
-    node.classList.add('added')
-  })
-})
+$('.link-title,.btm').show();
+});
 
-// Toggle show/hide dropdown
-document.querySelector('.js-b-indicator').addEventListener('click', (e) => {
-  e.preventDefault()
-  if (!bookmarkNode.classList.contains('show')) {
-    showDropdown()
-  } else {
-    hideDropdown()
-  }
-})
-
-document.addEventListener('click', (e) => {
-  if (!bookmarkNode.contains(e.target)) {
-    hideDropdown()
-  }
-})
-
-document.querySelector('.js-b-close-mobile-dropdown').addEventListener('click', () => {
-  hideDropdown()
-})
-
-// Add/remove bookmark
-document.querySelectorAll('.js-b-additem').forEach(node => {
-  node.addEventListener('click', (e) => {
-    e.preventDefault()
-    let items = b.data.items
-    let link = node.getAttribute('data-link')
-    // Add item
-    if (!node.classList.contains('added')) {
-      if (items.filter(item => item.link === link).length > 0) {
-        return
-      }
-
-      node.classList.add('added')
-
-      items.push({
-        link: link,
-        title: node.getAttribute('data-title'),
-        date: node.getAttribute('data-date'),
-        site: node.getAttribute('data-site')
-      })
-
-      b.data.items = items
-    } else { // Remove item
-      items = b.data.items.filter(item => {
-        return item.link !== link
-      })
-
-      node.classList.remove('added')
-
-      b.data.items = items
+$(function() {
+  
+  // Dropdown toggle
+  $('.dropdown-toggle').click(function(){
+    $(this).next('.dropdown').toggle();
+  });
+  
+  $(document).click(function(e) {
+    var target = e.target;
+    if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+      $('.dropdown').hide();
     }
-
-    localStorage.setItem('bookmark_items', JSON.stringify(items))
-  })
-})
-
-// Remove bookmark in the dropdown list.
-document.addEventListener('click', (e) => {
-  if ( ! e.target.classList.contains('js-b-dropdown-remove-bookmark') ) {
-    return
-  }
-
-  e.preventDefault()
-
-  let link = e.target.getAttribute('data-id')
-  let items = b.data.items.filter(item => {
-    return item.link !== link
-  })
-
-  b.data.items = items
-
-  localStorage.setItem('bookmark_items', JSON.stringify(items))
-
-  let addedNode = document.querySelector(`[data-link="${link}"]`)
-  addedNode.classList.remove('added')
-})
+  });
+  
+  });
